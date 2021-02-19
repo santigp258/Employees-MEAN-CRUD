0e6e34fe0759b0fdb5534c53c082a884f1f7adb0
Santigp258 <santigp258@gmail.com>
2021-02-19 17:43:47 -0500

    install bootstrap and add bootstrap components

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeRoutingModule } from './employee-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { FormComponent } from './pages/form/form.component';
+import { CardComponent } from './pages/card/card.component';
+
+
+@NgModule({
+  declarations: [HomeComponent, FormComponent, CardComponent],
+  imports: [
+    CommonModule,
+    EmployeeRoutingModule
+  ]
+})
+export class EmployeeModule { }
